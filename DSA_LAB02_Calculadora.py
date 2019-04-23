56a1eb398db83ed617eb48bff13cf708a16c5cf7
Patrick <patrick.ribeiro@gmail.com>
2019-04-23 02:24:03 -0300

    Versão 0.2
    teste estudos

---
+++ DSA_LAB02_Calculadora.py
@@ -0,0 +1,38 @@
+# LAB 02 - PROJETO DE calculadora
+
+continuar = 'S'
+while continuar == 'S':
+    print('Selecione a operação que deseja realizar.')
+    print('1 - SOMA')
+    print('2 - SUBTRAÇÃO')
+    print('3 - MULTPLICAÇÃO')
+    print('4 - DIVISÃO')
+    print()
+    opcao = int(input('Digite sua opoção: '))
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+
+    def soma(num1, num2):
+        return num1 + num2
+
+    def subtracao(num1, num2):
+        return num1 - num2
+
+    def divisao(num1, num2):
+        return num1 // num2
+
+    def multiplic(num1, num2):
+        return num1 * num2
+
+    if opcao == 1:
+        print('A Soma entre os números {} e {} é igual a {}'.format(num1, num2, soma(num1, num2)))
+    elif opcao == 2:
+        print('A Subtração entre os números {} e {} é igual a {}'.format(num1, num2, subtracao(num1, num2)))
+    elif opcao == 3:
+        print('A Multiplicação de {} vezes {} é igual a {}'.format(num1, num2, multiplic(num1, num2)))
+    elif opcao == 4:
+        print('{} dividido por {} é igual a {}'.format(num1, num2, divisao(num1, num2)))
+    else:
+        print('Opção inválida.')
+    continuar = str(input('Deseja Continuar? [S/N]')).upper()
+
