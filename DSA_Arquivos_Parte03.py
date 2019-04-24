7d2102c2284ae064a1c1914cc34ee4f6e9384af6
Patrick <patrick.ribeiro@gmail.com>
2019-04-23 21:18:58 -0300

    Versão 0.4
    estudos

--- DSA_Arquivos_Parte03.py
+++ DSA_Arquivos_Parte03.py
@@ -1,2 +1,18 @@
 # IMPORTANDO UM DATASET COM PANDAS.
 
+import pandas as pd
+
+file_name = 'arquivos/binary.csv'
+
+df = pd.read_csv(file_name)
+
+print(df.head()) # apenas imprime as primeiras linhas
+
+
+# IMPORTANDO OUTRO ARQUIVO
+
+file2 = 'arquivos/salarios.csv'
+
+df = pd.read_csv(file2)
+
+print(df.head())
